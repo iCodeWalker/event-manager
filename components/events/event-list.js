875384d7c32445c256b646b99c0bf012177e652e
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-12 21:14:29 +0530

    Added : event detail page

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -7,7 +7,7 @@ const EventList = (props) => {
   const { items } = props;
   return (
     <div className={classes.list}>
-      {items.map((event) => (
+      {items?.map((event) => (
         <EventItem
           key={event.id}
           id={event.id}
--- pages/events/[eventId]/index.js
+++ pages/events/[eventId]/index.js
@@ -4,6 +4,8 @@ import { getEventById } from "@/dummy-data";
 import EventSummary from "@/components/event-detail/event-summary";
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventContent from "@/components/event-detail/event-content";
+import ErrorAlert from "@/components/ui/error-alert";
+import Button from "@/components/ui/button";
 
 const EventDetailPage = () => {
   const router = useRouter();
@@ -13,7 +15,22 @@ const EventDetailPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No Event Found</p>;
+    return (
+      <React.Fragment>
+        <ErrorAlert>No events found for choosen filter</ErrorAlert>
+        <div
+          className="center"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "20px",
+          }}
+        >
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </React.Fragment>
+    );
   }
 
   return (

7c1eb193d0d62d040f74685483a7fe9b75cb2ef5
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-13 13:18:25 +0530

    added : slug page for catching route

--- components/ui/button.js
+++ components/ui/button.js
@@ -4,10 +4,18 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 const Button = (props) => {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <span className={classes.btn}>{props.children}</span>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={props.link}>
-      <span className={classes.btn}>{props.children}</span>
-    </Link>
+    <button onClick={props.link} className={classes.btn}>
+      {props.children}
+    </button>
   );
 };
 

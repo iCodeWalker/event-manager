4ac2d63cb67f34683b48cc467558371940dffadb
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-08-13 13:17:55 +0530

    added : all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useRouter } from "next/router";
+
+import EventList from "@/components/events/event-list";
+import EventSearch from "@/components/events/event-search";
+import { getAllEvents } from "@/dummy-data";
 
 const AllEventPage = () => {
+  const allEvents = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
   return (
-    <div>
-      <h1>All Events Page</h1>
-    </div>
+    <React.Fragment>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={allEvents} />
+    </React.Fragment>
   );
 };
 
